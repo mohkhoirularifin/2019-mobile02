ae80407991d753381f2858499d3bfc928a23cb68
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-09-07 21:54:26 +0700

    Menambahkan declaraion button

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -14,16 +14,21 @@ public class MainActivity extends AppCompatActivity {
 
 	//  TODO: deklarasikan variabel di sini
 	int random, guess;
+	private EditText masukkanAngka;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// TODO: bind layout di sini
+        masukkanAngka = findViewById((R.id.number_input);
 	}
 
 	// TODO: generate angka random di sini
 	private void initRandomNumber() {
-		random = (int) (Math.random()*100);
+//		random = (int) (Math.random()*100);
+		Random random = new Random();
+		int rando = random.nextInt(101);
 	}
 
 	public void handleGuess(View view) {
